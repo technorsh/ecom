a1d4c1dadab6a38f185b7cac019752d4df4db995
technorsh <technorsh@gmail.com>
2021-10-14 15:27:10 +0530

    frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!!
         </a>
+        jkkj
       </header>
     </div>
   );
